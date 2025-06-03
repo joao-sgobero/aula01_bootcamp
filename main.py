6ecf90903ad6a0b5ac33735838505e931130749f
João Henrique Sgobero <joao.sgobero@gmail.com>
2025-06-02 22:14:09 -0400

    Segundo exercício resolvido

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 
 nome = input("Digite seu nome: ")
 print(len(nome))
+
+#Exercício 02: Crie um programa onde o usuário digita dois valores e aparece a soma desses valores
+
+valor_01 = int(input("Digite o primeiro valor: "))
+valor_02 = int(input("Digite o segundo valor: "))
+
+soma = valor_01 + valor_02
+print(soma)
